3fe4ab48c230d16976d70d1e2e0e12b4b2c836aa
Mathias Jaqué <mathiasjaque@hotmail.com>
2025-10-22 23:02:06 +0200

    Hyggelyd

--- Assets/Mathias/Scripts/AudioManager.cs
+++ Assets/Mathias/Scripts/AudioManager.cs
@@ -26,7 +26,7 @@ public class AudioManager : MonoBehaviour
     {
         
             backgroundMusic = FMODUnity.RuntimeManager.CreateInstance(FmodEvents.Instance.Fightmusic);
-            backgroundMusic.start();
+           // backgroundMusic.start();
          
 
     }
