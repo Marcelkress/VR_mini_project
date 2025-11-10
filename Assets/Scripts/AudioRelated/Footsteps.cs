cda83b0d1c40b586d8871d861273aa8218ae700d
Stonepaiste <mathiasjaque@hotmail.com>
2025-11-10 22:37:29 +0100

    Fmod

---
+++ Assets/Scripts/AudioRelated/Footsteps.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class Footsteps : MonoBehaviour
+{
+    
+    public GameObject footstepsSound;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
