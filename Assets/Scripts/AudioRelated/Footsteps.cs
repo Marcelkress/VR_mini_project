279036721c870d73eb1c512e38fba03029f4547c
Stonepaiste <mathiasjaque@hotmail.com>
2025-11-11 13:51:48 +0100

    Footsteps script

--- Assets/Scripts/AudioRelated/Footsteps.cs
+++ Assets/Scripts/AudioRelated/Footsteps.cs
@@ -1,21 +1,45 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using FMODUnity;
-using FMOD.Studio;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class Footsteps : MonoBehaviour
+
+public class SimpleFootstep : MonoBehaviour
 {
-    
-    public GameObject footstepsSound;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private InputActionProperty leftThumbstick; // assign your LeftHand Move action
+    [SerializeField] private StudioEventEmitter footstepEmitter;
+    [SerializeField] private float stepInterval = 0.5f; // seconds between steps when moving
+    [SerializeField] private float moveDeadzone = 0.1f;
+
+    private float stepTimer;
+
+    void OnEnable()
     {
-        
+        leftThumbstick.action.Enable();
+    }
+    
+    void OnDisable()
+    {
+        leftThumbstick.action.Disable();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 input = leftThumbstick.action.ReadValue<Vector2>();
+        bool isMoving = input.magnitude > moveDeadzone;
+
+        if (isMoving)
+        {
+            stepTimer -= Time.deltaTime;
+            if (stepTimer <= 0f)
+            {
+                footstepEmitter.Play();
+                stepTimer = stepInterval;
+            }
+        }
+        else
+        {
+            stepTimer = 0f; // reset timer when not moving
+        }
     }
 }
