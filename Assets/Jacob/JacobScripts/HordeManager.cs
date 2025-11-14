8ac5860d2bafcea38460900b4a14c9d3c67bface
JacobVigsoe <eh66xs@student.aau.dk>
2025-11-14 10:24:32 +0100

    monster spawning

--- Assets/Jacob/JacobScripts/HordeManager.cs
+++ Assets/Jacob/JacobScripts/HordeManager.cs
@@ -122,10 +122,10 @@ public class HordeManager : MonoBehaviour
 
     private IEnumerator specialHordeCoroutine()
     {
-        yield return SpawnHorde(0, 10, player.position, spawnRadius: 5f, spawnDistance: 10f);
+        yield return SpawnHorde(0, 10, spawnPoints[0].transform.position, spawnRadius: 5f, spawnDistance: 10f);
         yield return new WaitForSeconds(3f);
-        yield return SpawnHorde(1, 5, player.position, spawnRadius: 5f, spawnDistance: 10f);
+        yield return SpawnHorde(1, 5, spawnPoints[2].transform.position, spawnRadius: 5f, spawnDistance: 10f);
         yield return new WaitForSeconds(3f);
-        yield return SpawnHorde(1, 3, player.position, spawnRadius: 5f, spawnDistance: 10f);
+        yield return SpawnHorde(1, 3, spawnPoints[0].transform.position, spawnRadius: 5f, spawnDistance: 10f);
     }
 }
